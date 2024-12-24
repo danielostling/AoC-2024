4505c333b296368030d8a15d852a8ba1ca3b3ae1
Author <example@example.org>
2024-12-24 23:38:44 +0100

    Day 4 solutions.

--- day-4/day-4.lisp
+++ day-4/day-4.lisp
@@ -89,13 +89,12 @@
                diagonals))
     diagonals))
 
-(defun scan-left-to-right (arr)
+(defun scan-left-to-right (arr &optional (re "XMAS"))
   "Search array from left to right, row by row, for XMAS. Return number of
    hits as integer."
-  (let* ((xmas "XMAS")
-         (rows (array-dimension arr 0)))
+  (let ((rows (array-dimension arr 0)))
     (loop :for row :from 0 :below rows
-          :for matches = (cl-ppcre:count-matches xmas (get-row arr row))
+          :for matches = (cl-ppcre:count-matches re (get-row arr row))
           :summing matches)))
 
 (defun rotate-right (arr)
@@ -109,42 +108,113 @@
                               (aref arr row col))))
     new-arr))
 
-(defun scan-horiz-and-vert (arr)
+(defun scan-horiz-and-vert (arr &optional (re "XMAS"))
   "Scan left to right and then rotate the array and scan again. Doing this four
    times at rotation 0 degrees, 90 degrees, 180 degrees and 270 degrees, will
    cover horizontal and vertical cases, as well as backwards occurrances of
    XMAS. Return number of found cases of the string."
-  (let ((cur-arr (alexandria:copy-array arr)))
-    (loop :for angle :from 90 :to 360 :by 90
-          :do (setf cur-arr (rotate-right cur-arr))
-          :sum (scan-left-to-right cur-arr))))
+  (loop :for angle :from 0 :to 270 :by 90
+          :for cur-arr = (alexandria:copy-array arr) then (rotate-right cur-arr)
+          :sum (scan-left-to-right cur-arr re)))
 
-(defun scan-string-list (lst)
+(defun scan-string-list (lst &optional (re "XMAS"))
   "Scan string list for XMAS. Return number of hits."
-  (let ((xmas "XMAS"))
-    (loop :for elt :in lst
-          :summing (cl-ppcre:count-matches xmas elt))))
+  (loop :for elt :in lst
+        ;; :do (format t "scan-string-list part ~a for re ~a~%" elt re)
+        :summing (cl-ppcre:count-matches re elt)))
 
-(defun scan-diagonals (arr)
+(defun scan-diagonals (arr &optional (re "XMAS") (scan-function #'scan-string-list))
   "Get all diagonals of arr and scan them left to right. Then rotate the array,
    get the new diagonals, and scan again. Doing this four times at rotation 0
    degrees, 90 degrees, 180 degrees and 270 degrees, will cover all directional
    cases of XMAS. Return number of found cases of the string."
+  (loop :for angle :from 0 :to 270 :by 90
+          :for cur-arr = (alexandria:copy-array arr) then (rotate-right cur-arr)
+          :for diagonals = (get-diagonals cur-arr)
+          :for matches = (funcall scan-function diagonals re)
+          :sum (funcall scan-function (get-diagonals cur-arr) re)))
 
-  (let ((cur-arr (alexandria:copy-array arr)))
-    ;; Start at 90 degrees because of how loop directive order sets aggregation last.
-    (loop :for angle :from 90 :to 360 :by 90
-          :do (setf cur-arr (rotate-right cur-arr))
-          :sum (scan-string-list (get-diagonals cur-arr)))))
+(defun populate-window (arr window-rows window-cols row-start col-start)
+  "Copy a part of given array into a new array.
+   Start the copy at position (row-start col-start).
+   Note: no bounds checking is done."
+  (let ((window (make-array (list window-rows window-cols)
+                            :element-type (array-element-type arr))))
+    ;; This loops rows and cols in the coordinate system of the given array.
+    (loop :for arr-row :from row-start :below (+ row-start window-rows)
+          :for window-row = 0 :then (1+ window-row)
+          :do (loop :for arr-col :from col-start :below (+ col-start window-cols)
+                    :for window-col = 0 :then (1+ window-col)
+                    :do (setf (aref window window-row window-col)
+                                (aref arr arr-row arr-col))))
+    window))
 
-(defun test-arr ()
-  (make-array '(3 3)
-              :element-type 'character
-              :initial-contents '((#\A #\B #\C)
-                                  (#\D #\E #\F)
-                                  (#\G #\H #\I))))
+(defun make-windows (bigger-arr)
+  "Walk a (smaller) 3x3 square 'windows' across a bigger square, from left to
+   right, starting in top left corner. When right side of smaller square reaches
+   the right edge of the bigger square, move smaller square back to left edge of
+   bigger square, but one row down.  Keep going until smaller square reaches
+   lower right corner of the bigger square.
 
-(defun test-arr2()
+   Return a list of all the smaller window squares, which each smaller square is
+   a list of lists; The 3x3 square (where . represents a blank space)
+   M . S
+   . A .
+   M . S
+
+   is represented by
+   ((#\M #\  #\S)
+    (#\  #\A #\ )
+    (#\M #\  #\S))
+
+   Top left corner of the bigger square is at row 0 and column 0. Columns grow
+   to the right, rows grow down."
+
+  ;; The top left corner of the small square will move inside the bigger square,
+  ;; from first col to two cols less than the col count of the larger square.
+  ;;
+  ;; For rows, the same holds; small square moves from first row to row two less
+  ;; than the row count of the larger square.
+  ;;
+  ;; Note that it may look strange to the whole width and height of the smaller
+  ;; square to get stopping indices.  This works due to loop macro :below
+  ;; preposition. If bigger square is 10x10, and smaller square is 3x3, and lisp
+  ;; uses zero based array indices, then bigger square has columns 0 through 9,
+  ;; and rows 0 through 9.  The final column the smaller square top left corner
+  ;; should be at, is 7. Otherwise the smaller square would "stick out" of the
+  ;; right side of the bigger square. Same logic applies to rows.
+  ;;
+  ;; The :below preposition stops *before* 7, so at 6. And then it works.
+  (let ((bigger-rows (array-dimension bigger-arr 0))
+        (bigger-cols (array-dimension bigger-arr 1))
+        (smaller-square-rows 3)
+        (smaller-square-cols 3))
+    (loop
+      :for bigger-starting-row
+        :from 0 :below (- bigger-rows (1- smaller-square-rows))
+      :nconcing (loop
+                  :for bigger-starting-col
+                    :from 0 :below (- bigger-cols (1- smaller-square-cols))
+                  :collect (populate-window
+                            bigger-arr
+                            smaller-square-rows smaller-square-cols
+                            bigger-starting-row bigger-starting-col)))))
+
+(defun scan-x-mas (arr)
+  "Scan for MAS in arr.
+
+   Produce a list of 3x3 windows by sliding it across arr, left to right, and
+   top to bottom. For each window, scan diagonals in all directions for the
+   string 'MAS'. If 'MAS' is present as a cross (see example in make-windows
+   docstring), then the window is to be counted, else not.
+
+   Return number of such 'MAS' crosses found in arr, across all windows."
+  (let ((windows (make-windows arr)))
+    (loop :for window :in windows
+          :for mas-diagonals = (scan-diagonals window "MAS")
+          :counting (= mas-diagonals 2))))
+
+(defun test-arr()
   (make-array '(10 10)
               :element-type 'character
               :initial-contents '((#\. #\. #\. #\. #\X #\X #\M #\A #\S #\.)
@@ -158,6 +228,27 @@
                                   (#\. #\. #\M #\. #\M #\. #\M #\. #\M #\M)
                                   (#\. #\X #\. #\X #\. #\X #\M #\A #\S #\X))))
 
+(defun test-arr2()
+  (make-array '(10 10)
+              :element-type 'character
+              :initial-contents '((#\. #\M #\. #\S #\. #\. #\. #\. #\. #\.)
+                                  (#\. #\. #\A #\. #\. #\M #\S #\M #\S #\.)
+                                  (#\. #\M #\. #\S #\. #\M #\A #\A #\. #\.)
+                                  (#\. #\. #\A #\. #\A #\S #\M #\S #\M #\.)
+                                  (#\. #\M #\. #\S #\. #\M #\. #\. #\. #\.)
+                                  (#\. #\. #\. #\. #\. #\. #\. #\. #\. #\.)
+                                  (#\S #\. #\S #\. #\S #\. #\S #\. #\S #\.)
+                                  (#\. #\A #\. #\A #\. #\A #\. #\A #\. #\.)
+                                  (#\M #\. #\M #\. #\M #\. #\M #\. #\M #\.)
+                                  (#\. #\. #\. #\. #\. #\. #\. #\. #\. #\.))))
+
+(defun test-arr3()
+  (make-array '(3 3)
+              :element-type 'character
+              :initial-contents '((#\M #\A #\S)
+                                  (#\. #\A #\A)
+                                  (#\M #\M #\S))))
+
 (defun solve-part-1 (input)
   "Solve part 1 of puzzle."
   (+ (scan-horiz-and-vert input)
@@ -165,7 +256,7 @@
 
 (defun solve-part-2 (input)
   "Solve part 2 of puzzle."
-  )
+  (scan-x-mas input))
 
 (defun main (&optional (mode :full))
   "AoC 2024 day 2 solution.
