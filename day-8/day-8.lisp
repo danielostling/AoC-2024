ca5dc4fc16226240937398657c3af28730c4b364
Author <example@example.org>
2025-01-05 00:00:04 +0100

    Day 8, part way into reorganizing code for vecor juggling.

--- day-8/day-8.lisp
+++ day-8/day-8.lisp
@@ -80,100 +80,117 @@
                                       #'tuple-less-p)
                         :collect pairs)))
 
-(defun antinode-positions (antenna-pos1 antenna-pos2 mapping)
-  "Calculate positions of antinodes given antenna pair position and a mapping.
+;; (defun antinode-positions (antenna-pos1 antenna-pos2 mapping)
+;;   "Calculate positions of antinodes given antenna pair position and a mapping.
 
-   antenna-pos1 and antenna-pos2 are (row col) tuples representing antenna
-   positions.
+;;    antenna-pos1 and antenna-pos2 are (row col) tuples representing antenna
+;;    positions.
 
-   An antinode is described in the puzzle as:
-     '... , an antinode occurs at any point that is perfectly in line with two
-     antennas of the same frequency - but only when one of the antennas is twice
-     as far away as the other. This means that for any pair of antennas with the
-     same frequency, there are two antinodes, one on either side of them.'
+;;    An antinode is described in the puzzle as:
+;;      '... , an antinode occurs at any point that is perfectly in line with two
+;;      antennas of the same frequency - but only when one of the antennas is twice
+;;      as far away as the other. This means that for any pair of antennas with the
+;;      same frequency, there are two antinodes, one on either side of them.'
 
-   Tuples antenna-pos1 and antenna-pos2 are sorted, so that lowest row comes
-   first. If rows are equal, lowest col comes first. This eliminates some
-   positioning combinations.
+;;    Tuples antenna-pos1 and antenna-pos2 are sorted, so that lowest row comes
+;;    first. If rows are equal, lowest col comes first. This eliminates some
+;;    positioning combinations.
 
-   The following combinations are possible
-   Combination 1, p1 is above and left compared to p2.
-   +--------+
-   | p1     |
-   |        |
-   |     p2 |
-   +--------+
+;;    The following combinations are possible
+;;    Combination 1, p1 is above and left compared to p2.
+;;    +--------+
+;;    | p1     |
+;;    |        |
+;;    |     p2 |
+;;    +--------+
 
-   Combination 2, p1 is left of p2 and horizontally aligned to p2 (as row is
-   equal, col sorting puts p1 to the left).
-   +--------+
-   |        |
-   | p1  p2 |
-   |        |
-   +--------+
+;;    Combination 2, p1 is left of p2 and horizontally aligned to p2 (as row is
+;;    equal, col sorting puts p1 to the left).
+;;    +--------+
+;;    |        |
+;;    | p1  p2 |
+;;    |        |
+;;    +--------+
 
-   Combination 3, p1 is above and right compared to p2.
-   +--------+
-   |     p1 |
-   |        |
-   | p2     |
-   +--------+
+;;    Combination 3, p1 is above and right compared to p2.
+;;    +--------+
+;;    |     p1 |
+;;    |        |
+;;    | p2     |
+;;    +--------+
  
-   Combination 4, p1 is above p2 and vertically aligned to p2.
-   +--------+
-   |   p1   |
-   |        |
-   |   p2   |
-   +--------+"
-  (let* ((r1 (first antenna-pos1))
-         (c1 (second antenna-pos1))
-         (r2 (first antenna-pos2))
-         (c2 (second antenna-pos2))
-         (r-delta (abs (- r1 r2)))
-         (c-delta (abs (- c1 c2)))
-         (a1-r nil)
-         (a1-c nil)
-         (a2-r nil)
-         (a2-c nil)
-         (antinodes ()))
+;;    Combination 4, p1 is above p2 and vertically aligned to p2.
+;;    +--------+
+;;    |   p1   |
+;;    |        |
+;;    |   p2   |
+;;    +--------+"
+;;   (let* ((r1 (first antenna-pos1))
+;;          (c1 (second antenna-pos1))
+;;          (r2 (first antenna-pos2))
+;;          (c2 (second antenna-pos2))
+;;          (r-delta (abs (- r1 r2)))
+;;          (c-delta (abs (- c1 c2)))
+;;          (a1-r nil)
+;;          (a1-c nil)
+;;          (a2-r nil)
+;;          (a2-c nil)
+;;          (antinodes ()))
 
-    ;; Combination 1, p1 is above and left compared to p2.
-    (when (and (> c2 c1)
-               (> r2 r1))
-      (setf a1-c (- c1 c-delta)
-              a1-r (- r1 r-delta)
-              a2-c (+ c2 c-delta)
-              a2-r (+ r2 r-delta)))
+;;     ;; Combination 1, p1 is above and left compared to p2.
+;;     (when (and (> c2 c1)
+;;                (> r2 r1))
+;;       (setf a1-c (- c1 c-delta)
+;;               a1-r (- r1 r-delta)
+;;               a2-c (+ c2 c-delta)
+;;               a2-r (+ r2 r-delta)))
     
-    ;; Combination 2, p1 is left of p2 and horizontally aligned to p2.
-    (when (and (> c2 c1)
-               (= r2 r1))
-      (setf a1-c (- c1 c-delta)
-              a1-r r1
-              a2-c (+ c2 c-delta)
-              a2-r r1))
+;;     ;; Combination 2, p1 is left of p2 and horizontally aligned to p2.
+;;     (when (and (> c2 c1)
+;;                (= r2 r1))
+;;       (setf a1-c (- c1 c-delta)
+;;               a1-r r1
+;;               a2-c (+ c2 c-delta)
+;;               a2-r r1))
 
-    ;; Combination 3, p1 is above and right compared to p2.
-    (when (and (< c2 c1)
-               (> r2 r1))
-      (setf a1-c (+ c1 c-delta)
-              a1-r (- r1 r-delta)
-              a2-c (- c2 c-delta)
-              a2-r (+ r2 r-delta)))
+;;     ;; Combination 3, p1 is above and right compared to p2.
+;;     (when (and (< c2 c1)
+;;                (> r2 r1))
+;;       (setf a1-c (+ c1 c-delta)
+;;               a1-r (- r1 r-delta)
+;;               a2-c (- c2 c-delta)
+;;               a2-r (+ r2 r-delta)))
 
-    ;; Combination 4, p1 is above p2 and vertically aligned to p2.
-    (when (and (= c2 c1)
-               (> r2 r1))
-      (setf a1-c c1
-              a1-r (- r1 r-delta)
-              a2-c c2
-              a2-r (+ r2 r-delta)))
+;;     ;; Combination 4, p1 is above p2 and vertically aligned to p2.
+;;     (when (and (= c2 c1)
+;;                (> r2 r1))
+;;       (setf a1-c c1
+;;               a1-r (- r1 r-delta)
+;;               a2-c c2
+;;               a2-r (+ r2 r-delta)))
     
-    (when (in-bounds-p a1-r a1-c mapping)
-      (push (list a1-r a1-c) antinodes))
-    (when (in-bounds-p a2-r a2-c mapping)
-      (push (list a2-r a2-c) antinodes))    
-    antinodes))
+;;     (when (in-bounds-p a1-r a1-c mapping)
+;;       (push (list a1-r a1-c) antinodes))
+;;     (when (in-bounds-p a2-r a2-c mapping)
+;;       (push (list a2-r a2-c) antinodes))    
+;;     antinodes))
+
+
+(defun get-vector (p1 p2)
+  "Return a vector v such that p1 + v = p2.
+
+   p1 and p2 are (row col) tuples, row and col are integers."
+  (mapcar #'- p2 p1))
+
+(defun antinode-positions (antenna-pos1 antenna-pos2 mapping)
+  (let* ((rows (array-dimension 0 mapping))
+         (cols (array-dimension 1 mapping))
+         (vector-p1-to-p2 (get-vector antenna-pos2 antenna-pos1))
+         (vector-p2-to-p1 (get-vector antenna-pos1 antenna-pos2))
+         )
+
+    )
+  )
 
 (defun antinodes (freq mapping)
   "Return a list of (row col) pairs for each antinode for given frequency and
