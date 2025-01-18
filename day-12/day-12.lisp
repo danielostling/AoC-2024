a81069426528b4dddc20f1f85ee4293da143959d
Author <example@example.org>
2025-01-18 12:14:48 +0100

    Day 12, solution for part 1.

--- day-12/day-12.lisp
+++ day-12/day-12.lisp
@@ -1,5 +1,16 @@
 ;;;; day-12.lisp
 
+;;;; A few definitions
+;;;; -----------------
+;;;; GARDEN   - a 2D grid representing plots where PLANTS grow. Each (row col)
+;;;;            POSITION in the GARDEN has a PLOT where a PLANT grows.
+;;;; PLOT     - a position in the GARDEN where a PLANT can grow.
+;;;; PLANT    - plants are represented by a single character.
+;;;; POSITION - a (row col) tuple, the coordinates for a PLOT.
+;;;; REGION   - a list of PLOTS where the same PLANTS grow, and each PLOT is
+;;;;            horizontally or vertically right next to another PLOT with the
+;;;;            same PLANT.
+
 (in-package #:day-12)
 
 (defun read-input (path)
@@ -8,21 +19,143 @@
 
 (defun parse-input (lines)
   "Parse INPUT, a list of strings, into solution-friendly format.
-
    Input represents a 2D grid of plant plot indicators. Return an array with two
    dimensions, where (row col) position (0 0) is 'top-left' in the input, and
    each value in the array is the corresponding character from the input."
   (let ((rows (length lines))
         (cols (length (first lines))))
     (make-array `(,rows ,cols)
-     :element-type 'character :adjustable nil  :fill-pointer nil :displaced-to nil
-     :initial-contents (mapcar (lambda (line) (coerce line 'list)) lines))))
+     :element-type 'character :adjustable nil :fill-pointer nil :displaced-to nil
+     :initial-contents (mapcar (lambda (line)
+                                 (coerce line 'list))
+                               lines))))
 
-(defun solve-part-1 (input)
+(defun plant-list (garden)
+  "Given a GARDEN, return a list of unique plants."
+  (let ((plants nil))
+    (dotimes (idx (1- (array-total-size garden)))
+      (pushnew (row-major-aref garden idx) plants :test #'char=))
+    plants))
+
+(defun plant-plots (plant garden)
+  "Given a PLANT and a GARDEN, return a list of positions for plots
+   growing the PLANT."
+  (destructuring-bind (rows cols) (array-dimensions garden)
+    (loop :for row :from 0 :below rows
+          :nconc (loop :for col :from 0 :below cols
+                       :when (char= (aref garden row col) plant)
+                       :collect `(,row ,col))) ))
+
+(defun inside-p (plot garden)
+  "Return T if PLOT is inside GARDEN, else NIL."
+  (destructuring-bind (rows cols) (array-dimensions garden)
+    (declare (ignore rows))
+    (destructuring-bind (row col) plot
+      (and (>= row 0) 
+           (>= col 0)
+           (> (array-total-size garden) (+ (* row cols) col))))))
+
+(defun is-neighbor-p (a b)
+  "Return T if positions A and B are horizontally or vertically right next to each
+   other, else NIL. Does not take into account if positions are inside garden."
+  (destructuring-bind (row-a col-a) a
+    (destructuring-bind (row-b col-b) b
+      (or
+       (and (= (abs (- row-a row-b)) 1)
+            (= (abs (- col-a col-b)) 0))
+       (and (= (abs (- row-a row-b)) 0)
+            (= (abs (- col-a col-b)) 1))))))
+
+(defun possible-neighbors (plot)
+  "Return all possible neighbor plots for given PLOT as a list of plots. No bounds
+   checking against a garden."
+  (destructuring-bind (plot-row plot-col) plot
+    `((,(1+ plot-row) ,plot-col)
+      (,(1- plot-row) ,plot-col)
+      (,plot-row ,(1+ plot-col))
+      (,plot-row ,(1- plot-col)))))
+
+(defun neighbors (plot garden)
+  "Return a list of plots that are (valid) neighbors of PLOT in GARDEN."
+  (remove-if-not
+   (lambda (this-plot)
+     (inside-p this-plot garden))
+   (possible-neighbors plot)))
+
+(defun partition-one-region (positions)
+  "Find one region in POSITIONS, return (IN-REGION NOT-IN-REGION).
+   First position in POSITION is the STARTING-POSITION. IN-REGION is a list of
+   all positions in POSITION that are direct or indirect neighbors of
+   STARTING-POSITION. NOT-IN-REGION is a list of all positions in POSITION that
+   are *not* direct or indirect neighbors of STARTING-POSITION. It's basically a
+   kind of flood fill. This is *not* the most efficient implementation."
+  (let ((in-region nil)
+        (stack `(,(first positions))))
+    (loop
+      :when (null stack)
+        :return `(,in-region ,(set-difference positions in-region))
+      :do (progn
+            (let ((current-position (pop stack)))
+              (pushnew current-position in-region :test #'equal)
+              (dolist (neighbor
+                       (remove-if-not
+                        (lambda (test-position)
+                          (and
+                           (null (member test-position in-region :test #'equal))
+                           (is-neighbor-p test-position current-position)))
+                        positions))
+                (pushnew neighbor stack :test #'equal)
+                (pushnew neighbor in-region :test #'equal)))))))
+
+;;   0 1 2 3 4 5 6 7 8 9
+;;  +-------------------+
+;; 0|R R R R I I C C F F| 0
+;; 1|R R R R I I C C C F| 1
+;; 2|V V R R R C C F F F| 2
+;; 3|V V R C C C J F F F| 3
+;; 4|V V V V C J J C F E| 4
+;; 5|V V I V C C J J E E| 5
+;; 6|V V I I I C J J E E| 6
+;; 7|M I I I I I J J E E| 7
+;; 8|M I I I S I J E E E| 8
+;; 9|M M M I S S J E E E| 9
+;;  +-------------------+
+;;   0 1 2 3 4 5 6 7 8 9
+
+(defun plant-regions (plots)
+  "Given PLOTS, a list of plots in the garden where a certain plant grows,
+   return a list of regions."
+  (let ((remaining-positions (copy-list plots))
+        (regions nil))
+    (loop
+      :when (null remaining-positions)
+        :return regions
+      :do (destructuring-bind (in-region not-in-region)
+              (partition-one-region remaining-positions)
+            (push in-region regions)
+            (setf remaining-positions not-in-region)))))
+
+(defun region-cost (plant garden)
+  "Given a PLANT and a GARDEN, return region cost, which is a sum of area *
+   perimeter for each region for the PLANT in the GARDEN."
+  (let* ((plots (plant-plots plant garden))
+         (regions (plant-regions plots))
+         (total-cost 0))
+    (dolist (region regions)
+      (let ((region-area 0)
+            (region-perimeter 0))
+        (dolist (plot region)
+          (let* ((candidates (possible-neighbors plot))
+                 (non-region-neighbors (set-difference candidates region :test #'equal)))
+            (incf region-perimeter (length non-region-neighbors))
+            (incf region-area)))
+        (incf total-cost (* region-area region-perimeter))))
+    total-cost))
+
+(defun solve-part-1 (garden)
   "Solve part 1 of puzzle."
-
-  
-  )
+  (loop :for plant :in (plant-list garden)
+        :sum (region-cost plant garden)))
 
 (defun solve-part-2 (input)
   "Solve part 2 of puzzle."
